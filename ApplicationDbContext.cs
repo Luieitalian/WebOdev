c16bc9c07c2602a2de98f55f5466d5141b4605e7
Luieitalian <enderbud125@hotmail.com>
2024-12-06 20:06:23 +0300

    Add project files.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebOdev
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=WebDB;Username=berkepite;Password=123");
+            }
+        }
+    }
+}
